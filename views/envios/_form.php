79037315f877fbcee68fd7912a28ab24f52bccfa
Daniel Gómez Vela <dani5002@hotmail.com>
2018-02-20 19:29:12 +0100

    Crea las noticias correctamente

--- views/envios/_form.php
+++ views/envios/_form.php
@@ -18,11 +18,11 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'entradilla')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'usuario_id')->textInput() ?>
+    <!-- <?= $form->field($model, 'usuario_id')->textInput() ?>
 
     <?= $form->field($model, 'created_at')->textInput() ?>
 
-    <?= $form->field($model, 'updated_at')->textInput() ?>
+    <?= $form->field($model, 'updated_at')->textInput() ?> -->
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
